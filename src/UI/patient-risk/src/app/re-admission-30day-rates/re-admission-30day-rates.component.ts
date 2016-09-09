3078d83436b92136391a5c238e948557457ac1c2
André Davis <cubicle.jockey@live.com>
2016-09-09 13:29:01 -0700

    Setting up for ReAdmission Charting

--- src/UI/patient-risk/src/app/re-admission-30day-rates/re-admission-30day-rates.component.ts
+++ src/UI/patient-risk/src/app/re-admission-30day-rates/re-admission-30day-rates.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ReAdmissionService } from '../services';
+import { ReAdmissionData } from '../models';
 
 @Component({
   moduleId: module.id,
   selector: 'pk-re-admission-30day-rates',
   templateUrl: 're-admission-30day-rates.component.html',
-  styleUrls: ['re-admission-30day-rates.component.css']
+  styleUrls: ['re-admission-30day-rates.component.css'],
+  providers: [ ReAdmissionService ]
 })
-export class ReAdmission30dayRatesComponent implements OnInit {
+export class ReAdmission30dayRatesComponent {
 
-  constructor() { }
+  constructor(private reAdmissionService: ReAdmissionService) {
 
-  ngOnInit() {
   }
-
 }
