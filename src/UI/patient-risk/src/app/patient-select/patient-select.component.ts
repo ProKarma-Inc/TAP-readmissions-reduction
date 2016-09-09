707f1d430bfcccebaca9c1da7836aa6df5b47a79
jozephbautista <jozeph.bautista@seattle.mantistgi.com>
2016-09-09 13:53:59 -0700

    Integreate new empty component readmission 30day rates

--- src/UI/patient-risk/src/app/patient-select/patient-select.component.ts
+++ src/UI/patient-risk/src/app/patient-select/patient-select.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { DischargePopulationComponent } from './discharge-population.component';
-import { ProportionAtRiskComponent } from './proportion-at-risk.component';
 
 @Component({
   moduleId: module.id,
   selector: 'pk-patient-select',
   templateUrl: 'patient-select.component.html',
-  directives: [DischargePopulationComponent, ProportionAtRiskComponent]
+  directives: [DischargePopulationComponent]
 })
 export class PatientSelectComponent implements OnInit {
 
